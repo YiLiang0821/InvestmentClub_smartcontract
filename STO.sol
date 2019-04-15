b31b87fad36636c543dfc96b5be4040bf3c94f47
yiliang <okc1994821@gmail.com>
2019-04-15 21:19:25 +0800

    STO contract

---
+++ STO.sol
@@ -0,0 +1,13 @@
+pragma solidity^0.4.25;
+
+contract STO{
+    constructor() {
+        
+    }
+    function getBalance() view returns(uint){
+      return this.balance;
+    } 
+    
+    function() payable {
+    }
+}
